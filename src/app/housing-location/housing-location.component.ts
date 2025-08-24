b458f566d44993ae00c05063e39d4453772f5bdc
dilshodturab <avlomberdioglid@gmail.com>
2025-08-24 10:15:22 +0500

    first commit

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-housing-location",
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    
+  `,
+  styleUrls: ["./housing-location.component.css"],
+})
+export class HousingLocationComponent { }
