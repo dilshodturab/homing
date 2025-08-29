277997516c2e6487d1bde1fd69480ed712986f27
dilshodturab <avlomberdioglid@gmail.com>
2025-08-29 20:25:06 +0500

    details upade

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,18 +3,43 @@ import { CommonModule } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
 import { HousingService } from "../housing.service";
 import { HousingLocation } from "../housinglocation";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-details",
   standalone: true,
-  imports: [CommonModule],
-  template: ` <p>details works! {{ housingLocation?.id }}</p> `,
+  imports: [CommonModule, ReactiveFormsModule],
+  template: `
+    <article>
+      <img class="listing-photo" [src]="housingLocation?.photo" />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">
+          {{ housingLocation?.city }}, {{ housingLocation?.state }}
+        </p>
+        <ul>
+          <li>Units available: {{ housingLocation?.availableUnits }}</li>
+          <li>Does this location has wifi: {{ housingLocation?.wifi }}</li>
+          <li>
+            Does this location has laundry: {{ housingLocation?.laundry }}
+          </li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <button class="primary" type="button">Apply now</button>
+      </section>
+    </article>
+  `,
   styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(""),
+  });
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params["id"]);
